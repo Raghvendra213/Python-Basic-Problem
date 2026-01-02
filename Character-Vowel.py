56c5da36b62ca5582d831643ca8540c86f1a0b90
Raghvendra213 <raghvendra153@gmail.com>
2026-01-02 19:55:35 +0530

    add vowel and constant

---
+++ Character-Vowel.py
@@ -0,0 +1,7 @@
+# Character Vowel hai or Constant
+ch = input("Enter a alpha:-").lower()
+# ch = input().lower()
+if ch in "aeiou":
+    print("Vowel")
+else:
+    print("Constant")
---
+++ conditonal-Statement/Divisible-Number.py
@@ -0,0 +1,7 @@
+# Number Divisible by 5 and 11
+
+num = int(input("Enter a number:-"))
+if num%5==0 and num%11 ==0:
+    print("Yes")
+else:
+    print("No")
