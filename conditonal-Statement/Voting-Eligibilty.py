f999db7a766ceeab20b98cb3c45d839a4779c8d1
Raghvendra213 <raghvendra153@gmail.com>
2026-01-02 19:24:16 +0530

    eligible

---
+++ conditonal-Statement/Voting-Eligibilty.py
@@ -0,0 +1,7 @@
+# Eligible for Vote
+
+n = int(input("Enter a number:-"))
+if n>=18:
+    print("Eligible")
+else:
+    print("Not eligible")
---
+++ conditonal-Statement/leap-year.py
@@ -0,0 +1,7 @@
+# Leap year
+
+year = eval(input("Enter a year:-"))
+if (year%400==0) or (year%4==0 and year %100!=0):
+    print("Leap Year")
+else:
+    print("Not leap year")
